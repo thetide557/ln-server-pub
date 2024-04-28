c5701b14c6c045fe416ea0612f5f7391c8e35546
fuleibin <1162450228@qq.com>
2024-04-28 16:31:15 +0800

    提交

--- src/pages/sxxc/inspectionList/components/inspectionForm/index.tsx
+++ src/pages/sxxc/inspectionList/components/inspectionForm/index.tsx
@@ -314,7 +314,7 @@ const InspectionForm = React.forwardRef<ReactNode, InspectionFormProps>((props, 
                 )}
                 {scope == 3 && (
                     <Form.Item name="hosts" rules={[{ required: true, message: '请选择Ip' }]}>
-                        <Checkbox.Group style={{ marginLeft: '65px' }} options={ipList.map(item => { return { label: item.ident, value: item.ident } })} />
+                        <Checkbox.Group style={{ marginLeft: '65px' }} options={ipList.map(item => { return { label: item.ip, value: item.ip } })} />
                     </Form.Item>
                 )}
             </Form>
