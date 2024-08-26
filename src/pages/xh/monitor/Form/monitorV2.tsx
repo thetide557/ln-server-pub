15fb544fe865aff99cdab0cb66ba5e71dccc1493
fleib <1162450228@qq.com>
2024-08-26 17:34:21 +0800

    update

--- src/pages/xh/monitor/Form/monitorV2.tsx
+++ src/pages/xh/monitor/Form/monitorV2.tsx
@@ -141,38 +141,43 @@ export default function () {
       }
       let typeValues = {};
       if (expands != null && expands.length > 0) {
-        const map = new Map();
-        expands.forEach((item, index, arr) => {
-          if (!map.has(item.config_category)) {
-            map.set(
-              item.config_category,
-              arr.filter((a) => a.config_category == item.config_category),
-            );
-          }
-        });
-        //以上分组加载数据
-        map.forEach(function (value, key) {
-          const formDataMap = new Map();
-          value.forEach((item, index, arr) => {
-            if (!formDataMap.has(item.group_id)) {
-              formDataMap.set(
-                item.group_id,
-                arr.filter((a) => a.group_id == item.group_id),
+        try {
+          const map = new Map();
+          expands.forEach((item, index, arr) => {
+            if (!map.has(item.config_category)) {
+              map.set(
+                item.config_category,
+                arr.filter((a) => a.config_category == item.config_category),
               );
             }
           });
-          let group: any = [];
-          formDataMap.forEach(function (value, i) {
-            let itemsChars = '';
+          //以上分组加载数据
+          map.forEach(function (value, key) {
+            const formDataMap = new Map();
             value.forEach((item, index, arr) => {
-              itemsChars += '"' + item.name + '":"' + item.value + '",';
+              if (!formDataMap.has(item.group_id)) {
+                formDataMap.set(
+                  item.group_id,
+                  arr.filter((a) => a.group_id == item.group_id),
+                );
+              }
             });
-            itemsChars = '{' + itemsChars.substring(0, itemsChars.length - 1) + '}';
-            group.push(JSON.parse(itemsChars));
+            let group: any = [];
+            formDataMap.forEach(function (value, i) {
+              let itemsChars = '';
+              value.forEach((item, index, arr) => {
+                itemsChars += '"' + item.name + '":"' + item.value + '",';
+              });
+              itemsChars = '{' + itemsChars.substring(0, itemsChars.length - 1) + '}';
+              group.push(JSON.parse(itemsChars));
+            });
+            typeValues[key] = group;
           });
-          typeValues[key] = group;
-        });
-        delete dat.exps;
+          delete dat.exps;
+        } catch (error) {
+          console.log('catch', error);
+          delete dat.exps;
+        }
       }
       let everyTypeValues = new Array();
       typeGroup.forEach((type) => {
