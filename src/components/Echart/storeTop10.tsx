b234d3c0bb7dd38a578fbbf1eae0fc69bd013276
weiwei <583781673@qq.com>
2024-12-03 14:07:21 +0800

    feat: 服务器内存利用率修改

--- src/components/Echart/storeTop10.tsx
+++ src/components/Echart/storeTop10.tsx
@@ -7,6 +7,7 @@ import moment from 'moment';
 export default function StoreTop10() {
     const [option, setOption] = useState({});
     const chartRef = useRef(null);
+    let isMounted = true;
     let options = {
         tooltip: {
             trigger: 'axis',
@@ -60,33 +61,47 @@ export default function StoreTop10() {
             },
         ]
     };
-    const getData = (() => {
-        const parsedRange = parseRange({ end: "now", start: "now-1h" });
-        let start = moment(parsedRange.start).unix();
-        fetchHistoryInstantBatch({
-            datasource_id: 1, queries: [{ time: start, query: "topk(10,mem_available_percent)" }]
-        }, 22).then((res) => {
-            res.dat[0].forEach(element => {
-                options.yAxis.data.push(element.metric.asset_name)
-                options.series[0].data.push(Number(element.value[1]).toFixed(3))
-            });
-            setTimeout(() => {
-                setOption(options)
-            }, 50)
 
-        });
-    })
     useEffect(() => {
+        const controller = new AbortController();
+        const getData = (() => {
+            const parsedRange = parseRange({ end: "now", start: "now-1h" });
+            let start = moment(parsedRange.start).unix();
+            fetchHistoryInstantBatch({
+                datasource_id: 1, queries: [{ time: start, query: "topk(10,mem_available_percent)" }]
+            }, 22).then((res) => {
+                res.dat[0].forEach(element => {
+                    options.yAxis.data.push(element.metric.asset_name)
+                    options.series[0].data.push(Number(element.value[1]).toFixed(3))
+                });
+                if (isMounted) {
+                    setOption(options)
+                    updateChart(options);
+                }
+            }).catch(error => {
+                console.error('Fetch error:', error);
+            });
+        })
         getData();
+        return () => {
+            isMounted = false;
+            controller.abort();
+            if (chartRef.current) {
+                echarts.dispose(chartRef.current);
+            }
+        };
     }, []);
-    useEffect(() => {
+    const updateChart = (option) => {
         if (chartRef.current) {
-            chartRef.current.getEchartsInstance().setOption(option);
+            const myChart = echarts.getInstanceByDom(chartRef.current);
+            if (myChart) {
+                myChart.setOption(option);
+            }
         }
-    }, [option]);
+    };
     return (
         <div className='box'>
-            <ReactEcharts ref={chartRef} notMerge={true} option={options} style={{ width: '100%', height: '22vh', margin: ' 0 auto' }}></ReactEcharts>
+            <ReactEcharts ref={chartRef} notMerge={true} option={option} style={{ width: '100%', height: '22vh', margin: ' 0 auto' }}></ReactEcharts>
         </div>
     )
 }
