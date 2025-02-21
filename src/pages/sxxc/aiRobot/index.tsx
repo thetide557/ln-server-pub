b1f74fc24e0b3ccadf7e090e4cf8d1caa7d44c34
fleib <1162450228@qq.com>
2025-02-21 15:49:45 +0800

    ai添加markdown文本读取

--- src/pages/sxxc/aiRobot/index.tsx
+++ src/pages/sxxc/aiRobot/index.tsx
@@ -3,7 +3,8 @@ import React, { useRef, useEffect, useState } from 'react';
 import Draggable from 'react-draggable';
 import { useHistory, useLocation } from 'react-router-dom';
 import { Input, Form } from 'antd';
-import { CloseOutlined, SyncOutlined  } from '@ant-design/icons';
+import { CloseOutlined, SyncOutlined } from '@ant-design/icons';
+import { marked } from 'marked';
 import './index.less'
 
 const AiRobot = function () {
@@ -17,6 +18,7 @@ const AiRobot = function () {
   const textAreaRef = useRef<any>(null);
   const location = useLocation();
   const { pathname } = location;
+  const [loading, setLoading] = useState<any>(false)
   // console.log('dsdf', pathname);
   let isScreen = true
   if (pathname.startsWith('/screenView')) {
@@ -25,8 +27,8 @@ const AiRobot = function () {
     isScreen = false
   }
   // console.log('is', isScreen);
-  
-  
+
+
   // useEffect(() => {
   //   // 路由变化时执行的代码
   //   console.log('当前路由:', location.pathname);
@@ -59,40 +61,41 @@ const AiRobot = function () {
   const fetchWithTimeout = (url, options = {}, timeout = 5000) => {
     const abortController = new AbortController();
     const signal = abortController.signal;
-  
+
     const fetchPromise = fetch(url, { ...options, signal });
-  
+
     const timeoutId = setTimeout(() => {
       abortController.abort();
       console.log('请求超时');
-      
+
     }, timeout);
-  
+
     return fetchPromise.finally(() => clearTimeout(timeoutId));
   }
 
-  const sendAi = (e:any) => {
+  const sendAi = (e: any) => {
     form.validateFields().then(async (values) => {
       console.log(values);
-      if (values.note.trim()) {
-        const data = {
-          content: values.note,
-          prompt: "",
-          kb_cls: "ln"
-        }
-        // 添加用户消息到消息列表
-        setAiMessages([...aiMessages, { text: values.note, sender: 'user' }, { text: '', sender: 'ai' }]);
-        form.setFieldsValue({ 'note': '' })
-        aiRef.current.scrollTop = aiRef.current.scrollHeight;
-        if (textAreaRef.current) {
-          e.preventDefault();
-          textAreaRef.current.selectionStart = 0;
-          textAreaRef.current.selectionEnd = 0;
-          textAreaRef.current.focus()
-        }
+      if (values.note.trim() && !loading) {
         try {
           let aiStr = ''
-          const response:any = await fetchWithTimeout('/chat', {
+          const data = {
+            content: values.note,
+            prompt: "",
+            kb_cls: "ln"
+          }
+          // 添加用户消息到消息列表
+          setAiMessages([...aiMessages, { text: values.note, sender: 'user' }, { text: '', sender: 'ai' }]);
+          form.setFieldsValue({ 'note': '' })
+          aiRef.current.scrollTop = aiRef.current.scrollHeight;
+          if (textAreaRef.current) {
+            e.preventDefault();
+            textAreaRef.current.selectionStart = 0;
+            textAreaRef.current.selectionEnd = 0;
+            textAreaRef.current.focus()
+          }
+          setLoading(true)
+          const response: any = await fetchWithTimeout('/chat', {
             method: 'POST',
             headers: {
               'Content-type': 'application/json'
@@ -101,6 +104,7 @@ const AiRobot = function () {
           }, 60000);
 
           if (!response.ok) {
+            setLoading(false)
             throw new Error(`HTTP error! Status: ${response.status}`);
           }
 
@@ -112,8 +116,9 @@ const AiRobot = function () {
             const { done, value } = await reader.read();
 
             if (done) {
-              setAiMessages([...aiMessages, { text: values.note, sender: 'user' }, { text: aiStr, sender: 'ai' }]);
+              setAiMessages([...aiMessages, { text: values.note, sender: 'user' }, { text: marked(aiStr), sender: 'ai' }]);
               console.log('Streaming finished.');
+              setLoading(false)
               return;
             }
 
@@ -123,19 +128,19 @@ const AiRobot = function () {
             const chunkText = textDecoder.decode(value);
             // console.log('Received text chunk:', chunkText);
             aiStr += chunkText
-            setAiMessages([...aiMessages, { text: values.note, sender: 'user' }, { text: aiStr, sender: 'ai' }])
+            setAiMessages([...aiMessages, { text: values.note, sender: 'user' }, { text: marked(aiStr), sender: 'ai' }])
             // console.log('str', aiStr);
-
             // 递归调用readStream以继续读取流
             readStream();
           }
           // 开始读取流
           readStream();
           // console.log('straaaaa', aiStr);
-          
+
         } catch (error) {
           setAiMessages([...aiMessages, { text: values.note, sender: 'user' }, { text: '访问超时', sender: 'ai' }]);
           console.error('Error streaming AI text:', error);
+          setLoading(false)
         }
 
       }
@@ -149,18 +154,18 @@ const AiRobot = function () {
 
   return (
     <>
-        {!pathname.startsWith('/login') && <Draggable bounds="parent" handle=".robot" onDrag={handleDrag} onStop={handleStop}>
+      {!pathname.startsWith('/login') && <Draggable bounds="parent" handle=".robot" onDrag={handleDrag} onStop={handleStop}>
         <div className='nav-bar'>
           {
-            aiShow && <div className="r-dialog" style={{background: (isScreen ? '#35649E' : '#fff')}}>
+            aiShow && <div className="r-dialog" style={{ background: (isScreen ? '#35649E' : '#fff') }}>
               <div className='ai-all'>
                 {
                   aiMessages.length == 0 ?
                     <div className="ai-top">
                       {
-                        isScreen ?  <img className='ai-logo' src="/image/ai/d-logo.png" alt="" /> :  <img className='ai-logo' src="/image/ai/l-logo.png" alt="" />
+                        isScreen ? <img className='ai-logo' src="/image/ai/d-logo.png" alt="" /> : <img className='ai-logo' src="/image/ai/l-logo.png" alt="" />
                       }
-                      <div className='ai-title'  style={{color: (isScreen ? '#fff' : '#333')}}>欢迎使用LingNiu，有什么可以帮助您？</div>
+                      <div className='ai-title' style={{ color: (isScreen ? '#fff' : '#333') }}>欢迎使用LingNiu，有什么可以帮助您？</div>
                     </div> : <div className="ai-content" ref={aiRef}>
                       <div className="messages">
                         {aiMessages.map((message, index) => (
@@ -170,8 +175,8 @@ const AiRobot = function () {
                                 <span className='user-text'>{message.text}</span>
                               </div> : <div className='ai-ai'>
                                 <img className='ai-lrobot' src='/image/ai/l-robot.png'></img>
-                                <div className={isScreen ? 'ai-answer dark-answer': 'ai-answer'}>
-                                  {message.text ? message.text : <SyncOutlined spin /> }
+                                <div className={isScreen ? 'ai-answer dark-answer' : 'ai-answer'}>
+                                  {message.text ? <div dangerouslySetInnerHTML={{ __html: message.text }}></div> : <SyncOutlined spin />}
                                 </div>
                               </div>}
                           </div>
@@ -190,7 +195,7 @@ const AiRobot = function () {
                   <Form.Item name="note">
                     <TextArea
                       ref={textAreaRef}
-                      className={isScreen ? 'ai-note dark-note': 'ai-note'}
+                      className={isScreen ? 'ai-note dark-note' : 'ai-note'}
                       placeholder="输入内容开始聊天"
                       onPressEnter={sendAi}
                     />
@@ -198,7 +203,9 @@ const AiRobot = function () {
                   <Form.Item>
                     <div className='ai-send'>
                       <div></div>
-                      <img onClick={sendAi} className='ai-icon' src="/image/ai/send.png" alt="submit" />
+                      {
+                        loading ? <SyncOutlined spin /> : <img onClick={sendAi} className='ai-icon' src="/image/ai/send.png" alt="submit" />
+                      }
                     </div>
                   </Form.Item>
                 </Form>
