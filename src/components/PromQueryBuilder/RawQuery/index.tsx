28776061585856745c7675651c1fe8123e1808de
魏伟 <weiwei1@xinchan.com.cn>
2025-04-01 09:14:45 +0800

    feat:还原

--- src/components/PromQueryBuilder/RawQuery/index.tsx
+++ src/components/PromQueryBuilder/RawQuery/index.tsx
@@ -67,8 +67,8 @@ function renderLabels(labels: PromVisualQueryLabelFilter[]) {
 }
 
 export function renderQuery(query: PromVisualQuery, nested?: boolean) {
-  // let queryString = `${query.metric ?? ''}${renderLabels(query.labels)}`;
-  let queryString = `${query.metric ?? ''}`;
+  let queryString = `${query.metric ?? ''}${renderLabels(query.labels)}`;
+  // let queryString = `${query.metric ?? ''}`;
   queryString = renderOperations(queryString, query.operations);
 
   if (!nested && hasBinaryOp(query) && Boolean(query.binaryQueries?.length)) {
