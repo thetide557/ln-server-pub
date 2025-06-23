257bd35a68acce11fc96a77485e9799afe2e507b
付磊斌 <fuleibin@xinchan.com.cn>
2025-06-23 14:41:43 +0800

    隐藏物联网与作业计划菜单

--- src/components/menu/topMenuXH.tsx
+++ src/components/menu/topMenuXH.tsx
@@ -35,11 +35,11 @@ const getMenuList = (t) => {
           icon: <IconFont type='icon-Menu_Infrastructure' />,
           label: t('运维资产清单'),
         },
-        {
-          key: '/xh/iotassetmgt',
-          icon: <IconFont type='icon-Menu_Infrastructure' />,
-          label: t('物联网资产管理'),
-        },
+        // {
+        //   key: '/xh/iotassetmgt',
+        //   icon: <IconFont type='icon-Menu_Infrastructure' />,
+        //   label: t('物联网资产管理'),
+        // },
         // {
         //   key: '/serverVideoAsset',
         //   icon: <IconFont type='icon-Menu_Infrastructure' />,
@@ -122,11 +122,11 @@ const getMenuList = (t) => {
           icon: <IconFont type='icon-Menu_Infrastructure' />,
           label: t('巡检管理'),
         },
-        {
-          key: '/productionplan',
-          icon: <IconFont type='icon-Menu_Infrastructure' />,
-          label: t('作业计划管理'),
-        },
+        // {
+        //   key: '/productionplan',
+        //   icon: <IconFont type='icon-Menu_Infrastructure' />,
+        //   label: t('作业计划管理'),
+        // },
       ],
     },
     {
