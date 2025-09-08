51ca41ff3ff393253e59cd0f4fe1e7ff486c061c
魏伟 <58781673@qq.com>
2025-09-08 17:20:19 +0800

    fix(deploymentManagement):部署管理列表分页修改

--- src/pages/sxxc/deploymentManagement/index.tsx
+++ src/pages/sxxc/deploymentManagement/index.tsx
@@ -51,7 +51,7 @@ export default function () {
   const { t } = useTranslation("assets");
   const [list, setList] = useState<any[]>([]);
   const [selectedAssets, setSelectedAssets] = useState<number[]>([]);
-  const [current, setCurrent] = useLocalStorage("asset_current_from", 1);
+  const [current, setCurrent] = useState(1);
   const [pageSize, setPageSize] = useLocalStorage("asset_current_page", 10);
   const [searchVal, setSearchVal] = useLocalStorage<any>(
     "asset_filter_value",
@@ -188,19 +188,6 @@ export default function () {
       persistenceKey: `dashboard-table-resizable-xh-asset-management`,
     },
   });
-
-  useEffect(() => {
-    const handleBeforeUnload = (event) => {
-      //event.preventDefault(); 
-      setCurrent(1);
-      return ''; 
-    };
- 
-    window.addEventListener('beforeunload', handleBeforeUnload);
-    return () => {
-      window.removeEventListener('beforeunload', handleBeforeUnload);
-    };
-  }, []);
 
   useEffect(() => {
     getTableData();
