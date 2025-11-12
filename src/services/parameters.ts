6407ec2e865860b62b99d8dbdf550ac85bb9b9ce
付磊斌 <fuleibin@xinchan.com.cn>
2025-11-12 11:10:45 +0800

    feat(user): 增强密码强度要求并修正API路径
    - 将密码最小长度从8位增加到12位
    - 更新密码复杂度校验提示信息
    - 移除用户配置接口路径中多余的斜杠('/')

--- src/services/parameters.ts
+++ src/services/parameters.ts
@@ -3,7 +3,7 @@ import { RequestMethod } from '@/store/common';
 import { N9EAPI } from '../../config/constant';
 //获取系统参数设置
 export const getParametersList = function () {
-  return request(`/api/n9e/user-config/`, {
+  return request(`/api/n9e/user-config`, {
       method: RequestMethod.Get
   })
 }
@@ -15,7 +15,7 @@ export const saveCustomerName = function (data) {
 };
 //更新系统参数设置
 export const updateParametersList = function (data) {
-  return request('/api/n9e/user-config/', {
+  return request('/api/n9e/user-config', {
     method: RequestMethod.Put,
     data
   });
