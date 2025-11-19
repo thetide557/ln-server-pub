aa416ac2b32ce2539548af5e0332524243f65296
付磊斌 <fuleibin@xinchan.com.cn>
2025-11-19 16:06:08 +0800

    feat(auth): 增强密码安全策略，最小长度从8位提升至12位

--- src/pages/account/changePassword.tsx
+++ src/pages/account/changePassword.tsx
@@ -33,7 +33,7 @@ export default function ChangePassword() {
   };
 
   const validatePassword = (_, value) => {
-    if (value && value.length >= 8) {
+    if (value && value.length >= 12) {
       const count = [/[a-z]/, /[A-Z]/, /\d/, /[!@#$%^&-*.]/].reduce((acc, regex) => {
         return acc + (regex.test(value) ? 1 : 0);
       }, 0);
@@ -41,7 +41,7 @@ export default function ChangePassword() {
         return Promise.resolve();
       }
     }
-    return Promise.reject('密码必须大于8位，并且包含大写字母、小写字母、数字和符号中的任意3种');
+    return Promise.reject('密码必须大于12位，并且包含大写字母、小写字母、数字和符号中的任意3种');
   };
 
   const modifyPassword = () => {
