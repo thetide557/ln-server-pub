629bd918109e4311c1ff3d47d02f834078ed8f27
yanbiao <yanbiao@xinchan.com.cn>
2026-07-31 10:02:20 +0800

    bugxiugai

--- src/components/Code/index.tsx
+++ src/components/Code/index.tsx
@@ -20,13 +20,13 @@ export default function Code(props: Props) {
     setCopied(true);
     if (!isValidElement(children) && typeof children === 'string') {
       copy2ClipBoard(children, true);
-    } else if (isValidElement(children) && typeof children.props.children === 'string') {
-      copy2ClipBoard(children.props.children, true);
+    } else if (isValidElement(children) && typeof (children as React.ReactElement).props.children === 'string') {
+      copy2ClipBoard((children as React.ReactElement).props.children as string, true);
     } else if (Array.isArray(children)) {
       let text = '';
       children.forEach((item: ReactNode) => {
-        if (isValidElement(item) && typeof item.props.children === 'string') {
-          text += item.props.children + '\n';
+        if (isValidElement(item) && typeof (item as React.ReactElement).props.children === 'string') {
+          text += (item as React.ReactElement).props.children + '\n';
         }
       });
       copy2ClipBoard(text, true);
